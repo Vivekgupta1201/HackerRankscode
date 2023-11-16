272fcf86178591469341b609a1ccef44683839e8
Vivek kumar <122138413+Vivekgupta1201@users.noreply.github.com>
2023-11-16 19:41:20 +0530

    Create Tag Content Extractor.java

---
+++ Object Oriented Programming/Tag Content Extractor.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution{
+	public static void main(String[] args){
+		
+		Scanner in = new Scanner(System.in);
+		int testCases = Integer.parseInt(in.nextLine());
+		while(testCases>0){
+			String line = in.nextLine();
+			
+          	//Write your code here
+               Pattern p = Pattern.compile("<(.+)>([^<]+)<(/\\1)>");
+        Matcher m = p.matcher(line);
+
+        boolean f = false;
+        while(m.find()) {
+            System.out.println(m.group(2));
+            f = true;
+        }
+
+        if(!f) {
+            System.out.println("None");
+        }
+        
+			
+			testCases--;
+		}
+	}
+}
+
+
+
